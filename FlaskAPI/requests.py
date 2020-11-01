0874f13c75e2524fa249af1a33df55559f3ce715
nathankouts <70446987+nathankouts@users.noreply.github.com>
2020-11-01 19:04:16 +0200

    Add files via upload

---
+++ FlaskAPI/requests.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  1 13:55:03 2020
+
+@author: Nathan
+"""
+
+import requests 
+from data_input import data_in
+
+URL = 'http://127.0.0.1:5000/predict'
+headers = {"Content-Type": "application/json"}
+data = {"input": data_in}
+
+r = requests.get(URL,headers=headers, json=data) 
+
+r.json()
---
+++ FlaskAPI/wsgi.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  1 12:21:48 2020
+
+@author: Nathan
+"""
+
+from app import app as application
+
+if __name__ == '__main__':
+    application.run()    
+    
